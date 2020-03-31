c7c7f316a3840350fc993361ed5ea884d9a6505b
Dmitrijs Zubriks <dima.zubrik@gmail.com>
2020-03-31 18:25:40 +0300

    Upload photo & profile photo & models

--- configuration.py
+++ configuration.py
@@ -1,3 +1,7 @@
+import pathlib
+
 SECRET_KEY = 'asdlkasdlkajsd'
 SQLALCHEMY_DATABASE_URI = 'sqlite:///instaclone.db'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+UPLOADS_DIRECTORY = pathlib.Path(__file__).parent / 'uploads'
--- views.py
+++ views.py
@@ -4,6 +4,8 @@ from flask.views import MethodView
 
 from flask_login import (
     login_user,
+    login_required,
+    current_user,
 )
 
 
@@ -12,8 +14,14 @@ from werkzeug.security import (
     check_password_hash,
 )
 
+from werkzeug.utils import secure_filename
+
 from database import db
-from models import User
+
+from models import (
+    User,
+    Photo,
+)
 
 
 def create_user(email, hashed_password):
@@ -26,7 +34,7 @@ def create_user(email, hashed_password):
     db.session.commit()
 
 
-class UserView(MethodView):
+class UserRegistrationView(MethodView):
     def get(self):
         return flask.render_template('registration.html')
 
@@ -69,3 +77,52 @@ class UserLoginView(MethodView):
 
         return 'Wrong Credentials'
 
+
+class UploadPhotoView(MethodView):
+    decorators = [
+        login_required,
+    ]
+
+    def get(self):
+        return flask.render_template('upload_photo.html')
+
+    def post(self):
+        photo_file = flask.request.files['photo']
+
+        secure_original_file_name = secure_filename(photo_file.filename)
+        file_name = flask.current_app.config['UPLOADS_DIRECTORY'] / secure_original_file_name
+
+        photo_file.save(str(file_name))
+
+        photo = Photo(
+            path=secure_original_file_name,
+            user_id=current_user.id,
+        )
+
+        db.session.add(photo)
+        db.session.commit()
+
+        photo_link = photo.photo_link()
+        response = flask.redirect(location=photo_link)
+
+        return response
+
+
+class ViewFile(MethodView):
+    def get(self, file_name):
+        uploads_directory = str(flask.current_app.config['UPLOADS_DIRECTORY'])
+
+        return flask.send_from_directory(uploads_directory, file_name)
+
+
+class UserProfileView(MethodView):
+    def get(self, user_id):
+        user = User.query.get(user_id)
+
+        if user is None:
+            return 'User not found!', 404
+
+        return flask.render_template(
+            template_name_or_list='profile_photos.html',
+            photos=user.photos,
+        )
